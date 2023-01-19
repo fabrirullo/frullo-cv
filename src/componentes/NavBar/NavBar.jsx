5e6905a571948f565612fc22fd2ac4d076545678
fabrirullo <fabriziorullo@hotmail.com>
2023-01-19 20:28:04 -0300

    finish

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -17,17 +17,17 @@ function NavBar() {
     return (
         <div className="containerNav">
             <nav class="navbar justify-content-cente">
-                <ul class="nav">
+                <ul class="nav nav-pills nav-fill">
                     <li class="nav-item">
                         <a style={{ color: location.pathname === "/#home" ? '#2EB086' : undefined }} id="/" class="nav-link" href="#home">Home</a>
                     </li>
-                    <li class="nav-item2">
+                    <li class="nav-item">
                         <a style={{ color: location.pathname === "/#sobremi" ? '#2EB086' : undefined }} id="/sobremi" class="nav-link" href="#sobremi">Sobre Mi</a>
                     </li>
-                    <li class="nav-item3">
+                    <li class="nav-item">
                         <a style={{ color: location.pathname === "/habilidades" ? '#2EB086' : undefined }} id="/habilidades" class="nav-link" href="#habilidades">Habilidades</a>
                     </li>
-                    <li class="nav-item4">
+                    <li class="nav-item">
                         <a style={{ color: location.pathname === "/proyectos" ? '#2EB086' : undefined }} class="nav-link" href="#proyectos">Proyectos</a>
                     </li>
                 </ul>
